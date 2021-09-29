388dcd826d6833cb877b77c7f059b701ff35b108
micaela <you@example.com>
2021-09-29 00:20:48 -0300

    inmem items repository

--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Models;
+
 namespace Catalog.Repositories
 {
     public class InMemItemsRepository
     {
-        
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
     }
 }
