8dd379a79408dd028b35478b51ce9967a89773c7
Micaela Pardini <micaela.mqs@gmail.com>
2021-10-07 12:17:29 -0300

    unit tests env

---
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Catalog.UnitTests
+{
+    public class ItemsControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
